4f2a894a8f17f3731db9e7707f97822df0b1f73f
jonathan parisot <parisot.jonathan@gmail.com>
2017-03-10 12:02:58 +0100

    solved the dose creation problem

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -7,26 +7,13 @@ class DosesController < ApplicationController
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(doses_params)
-    @dose.cocktail_id = @cocktail.id
-    @dose.save
-    if !(params[:dose][:ingredient] == "") # returning false whatever
-      @dose.ingredient = Ingredient.find(params[:dose][:ingredient])
-      if @dose.valid?
-        @dose.save
-        redirect_to dose_path(@dose)
-      else
-        render :new
-        # render 'cocktails/#{@cocktail.id}/doses/new'
-        # render new_cocktail_dose_path(@cocktail)
-      end
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
     else
-      # render 'cocktails/#{@cocktail.id}/doses/new'
+      # raise
       render :new
-      # render new_cocktail_dose_path(@cocktail)
-
     end
-
-
   end
 
   def destroy
@@ -43,3 +30,26 @@ class DosesController < ApplicationController
     params.require(:dose).permit(:ingredient_id, :description) # ingredient or ingredient_id?
   end
 end
+
+
+# def create
+#     @cocktail = Cocktail.find(params[:cocktail_id])
+#     @dose = Dose.new(doses_params)
+#     @dose.cocktail_id = @cocktail.id
+#     @dose.save
+#     if !(params[:dose][:ingredient] == "") # returning false whatever
+#       @dose.ingredient = Ingredient.find(params[:dose][:ingredient])
+#       if @dose.valid?
+#         @dose.save
+#         redirect_to dose_path(@dose)
+#       else
+#         render :new
+#         # render 'cocktails/#{@cocktail.id}/doses/new'
+#         # render new_cocktail_dose_path(@cocktail)
+#       end
+#     else
+#       # render 'cocktails/#{@cocktail.id}/doses/new'
+#       render :new
+#       # render new_cocktail_dose_path(@cocktail)
+
+#     end
